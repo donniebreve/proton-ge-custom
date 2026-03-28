7f1d3cd34ceaed723a0b1e892edfebe2b6cb75a2
donniebreve <donniebreve@protonmail.com>
2026-03-28 12:53:09 -0700

    Testing new patch 1

--- patches/protonprep-valve-staging.sh
+++ patches/protonprep-valve-staging.sh
@@ -235,10 +235,12 @@ apply_all_in_dir() {
     apply_patch "../patches/game-patches/silence-starcitizen-unsupported-os.patch"
     apply_patch "../patches/game-patches/eac_60101_timeout.patch"
 
-
     # https://github.com/JacKeTUs/wine/commits/lmu-d2d1-tinkering
     echo "WINE: -GAME FIXES- add le mans ultimate patches"
     apply_patch "../patches/game-patches/lemansultimate-gameinput.patch"
+
+    echo "WINE: -GAME FIXES- Death Stranding 2 netprofm hack"
+    apply_patch "../patches/game-patches/ds2-netprofm.patch"
 
 ### END GAME PATCH SECTION ###
 
